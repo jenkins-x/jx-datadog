4b07b6ecafab77840deffe91d902091d9bc6620e
Gareth Evans <gareth@bryncynfelin.co.uk>
2019-04-12 12:38:27 +0100

    feat: initial commit of dummy terraform files

---
+++ terraform/monitors.tf
@@ -0,0 +1,29 @@
+resource "datadog_monitor" "foo" {
+  name               = "Name for monitor foo"
+  type               = "metric alert"
+  message            = "Monitor triggered. Notify: @hipchat-channel"
+  escalation_message = "Escalation message @pagerduty"
+
+  query = "avg(last_1h):avg:aws.ec2.cpu{environment:foo,host:foo} by {host} > 4"
+
+  thresholds {
+    ok                = 0
+    warning           = 2
+    warning_recovery  = 1
+    critical          = 4
+    critical_recovery = 3
+  }
+
+  notify_no_data    = false
+  renotify_interval = 60
+
+  notify_audit = false
+  timeout_h    = 60
+  include_tags = true
+
+  silenced {
+    "*" = 0
+  }
+
+  tags = ["foo:bar", "baz"]
+}
